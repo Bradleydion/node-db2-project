ec17865edfca4da5a3c7458405cca2761a7d2c6b
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-03-01 17:32:02 -0500

    finished the project

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,43 @@
+const db = require('../../data/db-config')
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db.select("*").from('cars');
 }
 
-const getById = () => {
+
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db.select("*")
+  .from('cars')
+  .where("id", id)
+  .limit(1)
 }
 
-const create = () => {
+
+const create = async (cars) => {
   // DO YOUR MAGIC
+  const [id] = await db
+  .insert({
+    vin: cars.vin,
+    make: cars.make,
+    model: cars.model,
+    mileage: cars.mileage,
+    title: cars.title,
+    transmission: cars.transmission,
+  })
+  .into('cars')
+
+  const newCar = await db('cars')
+  .where("id", id)
+  .first()
+
+  return newCar
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
--- api/server.js
+++ api/server.js
@@ -3,5 +3,16 @@ const express = require("express")
 const server = express()
 
 // DO YOUR MAGIC
+const carsRouter = require("./cars/cars-router")
+
+server.use(express.json())
+server.use("/api/cars",carsRouter)
+
+server.use((err, req, res, next)=>{
+    console.log(err)
+    res.status(500).json({
+        message:"Molly, you in danger girl"
+    })
+})
 
 module.exports = server
